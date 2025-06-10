95061b3cb708b3b797d506312ced5b0f5bcbcc04
Libor Mitrenga <libor@mitrengovi.cz>
2025-06-10 22:44:08 +0200

    Přidány titleScreenMelody, screechSound a inGameMelody

--- app/audioManager.js
+++ app/audioManager.js
@@ -44,24 +44,193 @@ createAudioHandler(channel) {
   audioData(channel, sound, options) {
     var sampleRate = this.channels[channel].ctx.sampleRate;
     switch (sound) {
-      case 'tapePilotTone': return this.tapePilotToneData(sampleRate);
-      case 'tapeRndToneData': return this.tapeRndToneData(sampleRate);
-      case 'cycleBasicBeepsData': return this.cycleBasicBeepsData(sampleRate);
-      case 'pressKeyboardData': return this.pressKeyboardData(sampleRate);
+      case 'titleScreenMelody': return this.titleScreenMelody(sampleRate);
+      case 'screechSound': return this.screechSound(sampleRate);
+      case 'inGameMelody': return this.inGameMelody(sampleRate, options.lives);
+      case 'tapePilotToneSound': return this.tapePilotToneSound(sampleRate);
+      case 'tapeRndDataSound': return this.tapeRndDataSound(sampleRate);
+      case 'basicBeepsSound': return this.basicBeepsSound(sampleRate);
+      case 'keyboardSound': return this.keyboardSound(sampleRate);
      }
     return false;
   } // audioData
 
-  tapePilotToneData(sampleRate) {
+  extendArray(array, addition) {
+    var newArray = new Uint8Array(array.length+addition);
+    newArray.set(array, 0);
+    return newArray;
+  } // extendArray
+
+  resizeArray(array, length) {
+    var newArray = new Uint8Array(length);
+    newArray.set(array.slice(0, length), 0);
+    return newArray;
+  } // resizeArray
+
+  addPulse(frame, k, lastPos, fKeys, fragments, pulses, pulsesCounter) {
+    var newPos = Math.round(frame*k);
+    var pulse = newPos-lastPos;
+    if (!(pulse in (fKeys))) {
+      fKeys[pulse] = fragments.length;
+      fragments.push(pulse);
+    }
+    pulses[pulsesCounter] = fKeys[pulse];
+    return newPos;
+  } // addPulse
+
+  titleScreenMelody(sampleRate) {
+    var titleScreenTuneData = [
+      0x51,0x3C,0x33,0x51,0x3C,0x33,0x51,0x3C,0x33,0x51,0x3C,0x33,0x51,0x3C,0x33,0x51,0x3C,0x33,0x51,0x3C,0x33,0x51,0x3C,0x33,0x4C,0x3C,0x33,0x4C,0x3C,0x33,0x4C,0x39,0x2D,
+      0x4C,0x39,0x2D,0x51,0x40,0x2D,0x51,0x3C,0x33,0x51,0x3C,0x36,0x5B,0x40,0x36,0x66,0x51,0x3C,0x51,0x3C,0x33,0x51,0x3C,0x33,0x28,0x3C,0x28,0x28,0x36,0x2D,0x51,0x36,0x2D,
+      0x51,0x36,0x2D,0x28,0x36,0x28,0x28,0x3C,0x33,0x51,0x3C,0x33,0x26,0x3C,0x2D,0x4C,0x3C,0x2D,0x28,0x40,0x33,0x51,0x40,0x33,0x2D,0x40,0x36,0x20,0x40,0x36,0x3D,0x79,0x3D
+    ];
+
+    var fragments = [];
+    var fKeys = {};
+    var pulses = new Uint8Array(33000);
+    var pulsesCounter = 0;
+
+    var k = Math.round(sampleRate/860)/100;
+    var frame = 0;
+    var lastPos = -1;
+
+    for (var t = 0; t < titleScreenTuneData.length-3; t++) {
+      var b = 256;
+      var c = 100;
+      var e = titleScreenTuneData[t];
+      var d = e;
+      do {
+        do {
+          d--;
+          if (d == 0) {
+            d = e;
+            if (pulsesCounter == pulses.length) {
+              pulses = this.extendArray(pulses, 5000);
+            }
+            lastPos = this.addPulse(frame, k, lastPos, fKeys, fragments, pulses, pulsesCounter);
+            pulsesCounter++;
+          }
+          b--;
+          frame++;
+        } while (b > 0);
+        b = 256;
+        if (c == 50) {
+          e = e*2;
+        }
+        c--;
+      } while (c > 0);
+    }
+    pulses = this.resizeArray(pulses, pulsesCounter);
+    return {'fragments': fragments, 'pulses': pulses, 'volume': this.music};
+  } // titleScreenMelody
+
+  screechSound(sampleRate) {
+    var fragments = [];
+    var fKeys = {};
+    var pulses = new Uint8Array(4500);
+    var pulsesCounter = 0;
+
+    var k = Math.round(sampleRate/865)/100;
+    var frame = 0;
+    var lastPos = -1;
+
+    for (var t = 50; t < 81; t++) {
+      var a = t;
+      var e = a;
+      do {
+        var b = e;
+        do {
+          if (a == b) {
+            if (pulsesCounter == pulses.length) {
+              pulses = this.extendArray(pulses, 500);
+            }
+            lastPos = this.addPulse(frame, k, lastPos, fKeys, fragments, pulses, pulsesCounter);
+            pulsesCounter++;
+          }
+          frame++;
+          b--;
+        } while (b > 0);
+        a--;
+        if (pulsesCounter == pulses.length) {
+          pulses = this.extendArray(pulses, 500);
+        }
+        lastPos = this.addPulse(frame, k, lastPos, fKeys, fragments, pulses, pulsesCounter);
+        pulsesCounter++;
+      } while (a > 0);
+      frame = frame+1941;
+      if (pulsesCounter == pulses.length) {
+        pulses = this.extendArray(pulses, 500);
+      }
+      lastPos = this.addPulse(frame, k, lastPos, fKeys, fragments, pulses, pulsesCounter);
+      pulsesCounter++;
+    }
+    pulses = this.resizeArray(pulses, pulsesCounter);
+    return {'fragments': fragments, 'pulses': pulses, 'volume': this.music};
+  } // screechSound
+
+  inGameMelody(sampleRate, lives) {
+    var inGameTuneData = [
+      0x56,0x60,0x56,0x60,0x66,0x66,0x80,0x80,0x80,0x80,0x66,0x60,0x56,0x60,0x56,0x60,0x66,0x60,0x56,0x4C,0x48,0x4C,0x48,0x4C,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,
+      0x40,0x40,0x40,0x40,0x44,0x44,0x4C,0x4C,0x56,0x60,0x66,0x60,0x56,0x56,0x66,0x66,0x51,0x56,0x60,0x56,0x51,0x51,0x60,0x60,0x40,0x40,0x40,0x40,0x40,0x40,0x40,0x40
+    ];
+
+    var fragments = [];
+    var fKeys = {};
+    var pulses = new Uint8Array(1500);
+    var pulsesCounter = 0;
+
+    var k = Math.round(sampleRate/860)/100;
+    var frame = 0;
+    var lastPos = -1;
+    var m = 0;
+
+    for (var r = 0; r < 2; r++) {
+      for (var t = 0; t < inGameTuneData.length; t++) {
+        m++;
+        var n = (m&126)>>1;
+        var e = inGameTuneData[n]+28-4*lives;
+        var b = 256;
+        var c = 3;
+        do {
+          do {
+            e--;
+            if (e == 0) {
+              var e = inGameTuneData[n]+28-4*lives;
+              if (pulsesCounter == pulses.length) {
+                pulses = this.extendArray(pulses, 500);
+              }
+              lastPos = this.addPulse(frame, k, lastPos, fKeys, fragments, pulses, pulsesCounter);
+              pulsesCounter++;
+            }
+            b--;
+            frame++;
+          } while (b > 0)
+          b = 256;
+          c--;
+        } while (c > 0)
+        frame = frame+6700;
+        if (pulsesCounter == pulses.length) {
+          pulses = this.extendArray(pulses, 500);
+        }
+        lastPos = this.addPulse(frame, k, lastPos, fKeys, fragments, pulses, pulsesCounter);
+        pulsesCounter++;
+      }
+    }
+
+    pulses = this.resizeArray(pulses, pulsesCounter);
+    return {'fragments': fragments, 'pulses': pulses, 'volume': this.music};
+  } // inGameMelody
+
+  tapePilotToneSound(sampleRate) {
     // T-state is 1/3500000 = 0.0000002867 sec. 
     // leader pulse is 2168 T-states long and is repeated 8063 times for header blocks and 3223 times for data blocks
     var pulse = Math.ceil(sampleRate*2168/3500000);
     var fragments = [pulse];
     var pulses = [0];
     return {'fragments': fragments, 'pulses': pulses, 'volume': this.sounds};
-  } // tapePilotToneData
+  } // tapePilotToneSound
 
-  tapeRndToneData(sampleRate) {
+  tapeRndDataSound(sampleRate) {
     // two sync pulses of 667 and 735 T-states
     var f667 = Math.ceil(sampleRate*667/3500000);
     var f735 = Math.ceil(sampleRate*735/3500000);
@@ -72,9 +241,9 @@ createAudioHandler(channel) {
     var fragments = [f667, f735, f885, f1710];
     var pulses = [0, 0, 1, 1];
     return {'fragments': fragments, 'pulses': pulses, 'volume': this.sounds, 'infinityRndPulses': {'fragments': [2, 3], 'quantity': 2}};
-  } // tapeRndToneData
+  } // tapeRndDataSound
 
-  cycleBasicBeepsData(sampleRate) {
+  basicBeepsSound(sampleRate) {
     var beeps = [261.626, 293.665, 329.628, 369.994, 415.305, 466.164, 523.251];
     
     var fragments = [];
@@ -93,14 +262,14 @@ createAudioHandler(channel) {
       pulses.push(beeps.length);
     }
     return {'fragments': fragments, 'pulses': pulses, 'volume': this.sounds};
-  } // cycleBasicBeepsData
+  } // basicBeepsSound
 
-  pressKeyboardData(sampleRate) {
+  keyboardSound(sampleRate) {
     var pulse = Math.ceil(15*sampleRate/44100);
     var fragments = [pulse];
     var pulses = [0];
     return {'fragments': fragments, 'pulses': pulses, 'volume': this.sounds};
-  } // pressKeyboardData
+  } // keyboardSound
 
 } // class AudioManager
 
