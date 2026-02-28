d6677cd113c362827bf4597f59748bc3aded4428
Libor Mitrenga <libor@mitrengovi.cz>
2026-02-28 20:05:46 +0100

    touchesJoysticks je přemístěn do InputEventsManager

--- app/roomModel.js
+++ app/roomModel.js
@@ -28,7 +28,6 @@ export class RoomModel extends AbstractModel {
     this.autorepeatKeys = false;
     this.needDraw = true;
     this.safeInitPosition = false;
-    this.tsJoysticks = {};
 
     this.initData = {
       info: [
@@ -546,7 +545,7 @@ export class RoomModel extends AbstractModel {
         this.postWorkerMessage({id: 'controls', action: ts.action, value: true});
         break;
       case 'joystick':
-        this.tsJoysticks[event.identifier] = {center: {x: event.x, y: event.y}, action: false, control: ts.control, actions: ts.actions};
+        this.app.inputEventsManager.touchesJoysticks[event.identifier] = {center: {x: event.x, y: event.y}, action: false, control: ts.control, actions: ts.actions};
         break;
     }
   } // touchStart
@@ -558,39 +557,40 @@ export class RoomModel extends AbstractModel {
         this.postWorkerMessage({id: 'controls', action: ts.action, value: false});
         break;
       case 'joystick':
-        if (this.tsJoysticks[event.identifier].action !== false) {
-          this.postWorkerMessage({id: 'controls', action: this.tsJoysticks[event.identifier].action, value: false});
+        if (this.app.inputEventsManager.touchesJoysticks[event.identifier].action !== false) {
+          this.postWorkerMessage({id: 'controls', action: this.app.inputEventsManager.touchesJoysticks[event.identifier].action, value: false});
         }
-        delete this.tsJoysticks[event.identifier];
+        delete this.app.inputEventsManager.touchesJoysticks[event.identifier];
         break;
     }
   } // touchEnd
 
   touchMove(event) {
-    if (event.identifier in this.tsJoysticks) {
+    if (event.identifier in this.app.inputEventsManager.touchesJoysticks) {
       var action = false;
-      switch (this.tsJoysticks[event.identifier].control) {
+      var tsJoystick = this.app.inputEventsManager.touchesJoysticks[event.identifier];
+      switch (tsJoystick.control) {
         case 'horizontal':
-          if (event.x-this.tsJoysticks[event.identifier].center.x < 0) {
-            action = this.tsJoysticks[event.identifier].actions[0];
+          if (event.x-tsJoystick.center.x < 0) {
+            action = tsJoystick.actions[0];
           }
-          if (event.x-this.tsJoysticks[event.identifier].center.x > 0) {
-            action = this.tsJoysticks[event.identifier].actions[1];
+          if (event.x-tsJoystick.center.x > 0) {
+            action = tsJoystick.actions[1];
           }
           break;
         case 'vertical':
-          if (event.y-this.tsJoysticks[event.identifier].center.y < 0) {
-            action = this.tsJoysticks[event.identifier].actions[0];
+          if (event.y-tsJoystick.center.y < 0) {
+            action = tsJoystick.actions[0];
           }
-          if (event.y-this.tsJoysticks[event.identifier].center.y > 0) {
-            action = this.tsJoysticks[event.identifier].actions[1];
+          if (event.y-tsJoystick.center.y > 0) {
+            action = tsJoystick.actions[1];
           }
           break;
       }
-      if (this.tsJoysticks[event.identifier].action !== action) {
-        this.postWorkerMessage({id: 'controls', action: this.tsJoysticks[event.identifier].action, value: false});
-        this.tsJoysticks[event.identifier].action = action;
-        this.postWorkerMessage({id: 'controls', action: this.tsJoysticks[event.identifier].action, value: true});
+      if (tsJoystick.action !== action) {
+        this.postWorkerMessage({id: 'controls', action: tsJoystick.action, value: false});
+        tsJoystick.action = action;
+        this.postWorkerMessage({id: 'controls', action: tsJoystick.action, value: true});
       }
     }
   } // touchMove
